2c5b3d1feb11b91595d9e1084e13eeb2791273b2
Chris <chris.r.thomas@johnlewis.co.uk>
2021-03-02 08:20:41 +0000

    Update Package.swift file and tweak folder structure to match

--- Package.swift
+++ Package.swift
@@ -8,12 +8,26 @@ let package = Package(
     .macOS(.v10_10), .iOS(.v8), .tvOS(.v9)
   ],
   products: [
-    .library(name: "PactConsumerSwift", targets: ["PactConsumerSwift"])
+    .library(name: "PactConsumerSwift",
+             targets: [
+                "PactConsumerSwift"
+            ])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "1.1.0")),
+    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "7.0.0")),
   ],
   targets: [
     .target(
-      name: "PactConsumerSwift",
-      path: "./Sources"
+      name: "PactConsumerSwift"
+    ),
+    .testTarget(name: "PactConsumerSwiftTests",
+                dependencies: [
+                    .target(name: "PactConsumerSwift"),
+                    .product(name: "Quick", package: "Quick"),
+                    .product(name: "Nimble", package: "Nimble")
+                ]
     )
   ]
+
 )
